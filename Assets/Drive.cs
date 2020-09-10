c2bafd8cefc9f6b156cf5211edef6c34b114f3a5
Wirgiliusz <kamx98@gmail.com>
2020-09-10 20:09:06 +0200

    Working wheels
    Added basic car shape with 4 working wheels

---
+++ Assets/Drive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider WC;
+    public float torque = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WC = this.GetComponent<WheelCollider>();
+    }
+
+
+    void go(float accel) {
+        accel = Mathf.Clamp(accel, -1, 1);
+        float thrustTorque = accel * torque;
+        WC.motorTorque = thrustTorque;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        go(a);
+    }
+}
