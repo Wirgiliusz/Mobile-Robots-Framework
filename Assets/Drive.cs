785b322f7f747153ad2af57368ce8563ad0f55e6
Wirgiliusz <kamx98@gmail.com>
2020-09-13 21:04:57 +0200

    Added velocity to log

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Drive : MonoBehaviour
 {
     public WheelCollider WC;
-    public float torque = 200;
+    public float torque = 50;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,6 @@ public class Drive : MonoBehaviour
     {
         float acceleration = Input.GetAxis("Vertical");
         go(acceleration);
+        Debug.Log("Velocity: " + String.Format("{0:0.00}", this.GetComponentInParent<Rigidbody>().velocity.z) + " m/s");
     }
 }
