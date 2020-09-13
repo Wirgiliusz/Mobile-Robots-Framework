f697e60f3f47d181c1acadbe6a39339ec85ee587
Wirgiliusz <kamx98@gmail.com>
2020-09-13 21:31:42 +0200

    Added left and right wheel control

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -6,7 +6,9 @@ using System;
 public class Drive : MonoBehaviour
 {
     public WheelCollider WC;
-    public float torque = 50;
+    private float speed = 0;
+    private float speedR = 0;
+    private float speedL = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +16,29 @@ public class Drive : MonoBehaviour
         WC = this.GetComponent<WheelCollider>();
     }
 
-
-    void go(float accel) {
-        accel = Mathf.Clamp(accel, -1, 1);
-        float thrustTorque = accel * torque;
-        WC.motorTorque = thrustTorque;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        float acceleration = Input.GetAxis("Vertical");
-        go(acceleration);
-        Debug.Log("Velocity: " + String.Format("{0:0.00}", this.GetComponentInParent<Rigidbody>().velocity.z) + " m/s");
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            speed += 1;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            speed -= 1;
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            speed = 0;
+            speedR = 0;
+            speedL = 0;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            speedR += 1;
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            speedL += 1;
+        }
+
+        if (this.name == "WheelR") {
+            WC.motorTorque = speed + speedR;
+        } else if (this.name == "WheelL") {
+            WC.motorTorque = speed + speedL;
+        }
+
+        Debug.Log("Speed: "+ speed + " SpeedR: "+ speedR + " SpeedL: "+ speedL + " Velocity: " + String.Format("{0:0.00}", this.GetComponentInParent<Rigidbody>().velocity.magnitude) + " m/s");
     }
 }
