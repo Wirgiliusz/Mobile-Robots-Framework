d5baf0b35cd4b878eb365a631683c349ce0dc0c9
Wirgiliusz <kamx98@gmail.com>
2020-09-14 18:57:51 +0200

    Added comments

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -6,9 +6,16 @@ using System;
 public class Drive : MonoBehaviour
 {
     public WheelCollider WC;
-    private float speed = 0;
-    private float speedR = 0;
-    private float speedL = 0;
+
+    // Speeds of different wheels
+    private float speed = 0;    // speed of both wheels
+    private float speedR = 0;   // speed of right wheel
+    private float speedL = 0;   // speed of left wheel
+
+    // Value of speed change for different wheels
+    public float speed_change;
+    public float speedR_change;
+    public float speedL_change;
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +26,41 @@ public class Drive : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*  Wheels control keybinds
+        *   Up arrow:               + speed_change for both wheels  [/\]
+        *   Down arrow:             - speed_change for both wheels  [\/]
+        *   Left arrow:             + speedL_change for left wheel  [<-]
+        *   Shift + Left arrow:     - speedL_change for left wheel  [S + <-]
+        *   Right arrow:            + speedR_change for right wheel [->]
+        *   Shift + Right arrow:    - speedR_change for right wheel [S + ->]
+        *   Space:                  Reset all speeds to 0           [_]
+        */
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            speed += 1;
+            speed += speed_change;
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            speed -= 1;
+            speed -= speed_change;
         } else if (Input.GetKeyDown(KeyCode.Space)) {
             speed = 0;
             speedR = 0;
             speedL = 0;
         } else if (Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftShift)) {
-            speedR += 1;
+            speedR += speedR_change;
         } else if (Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.LeftShift)) {
-            speedL += 1;
+            speedL += speedL_change;
         } else if (Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftShift)) {
-            speedR -= 1;
+            speedR -= speedR_change;
         } else if (Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKey(KeyCode.LeftShift)) {
-            speedL -= 1;
+            speedL -= speedL_change;
         }
 
+        // Distinguish which wheel should get which speed change
         if (this.name == "WheelR") {
             WC.motorTorque = speed + speedR;
         } else if (this.name == "WheelL") {
             WC.motorTorque = speed + speedL;
         }
 
+        // Log different speeds
         Debug.Log("Speed: "+ speed + " SpeedR: "+ speedR + " SpeedL: "+ speedL + " Velocity: " + String.Format("{0:0.00}", this.GetComponentInParent<Rigidbody>().velocity.magnitude) + " m/s");
     }
 }
