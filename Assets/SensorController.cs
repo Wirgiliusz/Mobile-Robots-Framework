abbeaed4cb061b345bb82d8835161d1e8f428ccc
Wirgiliusz <kamx98@gmail.com>
2020-09-19 18:28:35 +0200

    Added max sensor distance and colors

--- Assets/SensorController.cs
+++ Assets/SensorController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class SensorController : MonoBehaviour
 {
-    private float hitDistance = -1;   
+    public float maxHitDistance;
+    public LineRenderer sensorLine;
+    private float hitDistance = -1f;   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sensorLine.SetPosition(1, transform.forward * maxHitDistance);
     }
 
     // Update is called once per frame
@@ -17,10 +20,14 @@ public class SensorController : MonoBehaviour
         RaycastHit hit;
         Ray downRay = new Ray(transform.position, transform.forward);
 
-        if (Physics.Raycast(downRay, out hit)) {
+        if (Physics.Raycast(downRay, out hit) && hit.distance <= maxHitDistance) {
             hitDistance = hit.distance;
+            sensorLine.startColor = Color.red;
+            sensorLine.endColor = Color.red;
         } else {
             hitDistance = -1f;
+            sensorLine.startColor = Color.yellow;
+            sensorLine.endColor = Color.yellow;
         }
     }
 
