38723f4dd031105955a43db5eac8b04b1b72fc60
Wirgiliusz <kamx98@gmail.com>
2020-09-19 19:22:07 +0200

    Changed program structure

---
+++ Assets/Scripts/MotorsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorsController : MonoBehaviour
+{
+    public WheelCollider WC;
+    public GameObject WheelModel;
+
+    private float speed = 0;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WC = this.GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateWheelModelRotation();
+    }
+
+
+    public void addSpeed(float new_speed) {
+        speed += new_speed;
+        addSpeedToWheel();
+    }
+
+    public void setSpeed(float new_speed) {
+        speed = new_speed;
+        addSpeedToWheel();
+    }
+
+    public void setBrake(bool state) {
+        if (state) {
+            WC.brakeTorque = Mathf.Infinity;
+        } else {
+            WC.brakeTorque = 0;
+        }
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    void addSpeedToWheel() {
+        WC.motorTorque = speed;
+
+    }
+
+    void updateWheelModelRotation() {
+        Quaternion rotation;
+        Vector3 position;
+        WC.GetWorldPose(out position, out rotation);
+        rotation = rotation * Quaternion.Euler(new Vector3(0, 0, 90));
+        WheelModel.transform.position = position;
+        WheelModel.transform.rotation = rotation;
+    }
+}
