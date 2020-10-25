0f61c3722a0430d1055885b6561170267d713896
Wirgiliusz <kamx98@gmail.com>
2020-10-25 22:09:23 +0100

    Added 4 sensors to robot

--- Assets/Scripts/SensorController.cs
+++ Assets/Scripts/SensorController.cs
@@ -13,7 +13,7 @@ public class SensorController : MonoBehaviour
     void Start()
     {
         sensorLine = this.GetComponent<LineRenderer>();
-        sensorLine.SetPosition(1, transform.forward * maxHitDistance * 66.666f);
+        sensorLine.SetPosition(1, Vector3.forward * maxHitDistance * 66.666f); //TODO Fix sensors ray visualisation
         sensorReady = false;
         hitDistance = 0;
     }
