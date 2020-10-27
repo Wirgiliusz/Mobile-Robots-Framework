de6844881ffb16a2711e33bf28c3d9632901251a
Wirgiliusz <kamx98@gmail.com>
2020-10-27 23:29:00 +0100

    Added rotation with PID

--- Assets/Scripts/PIDController.cs
+++ Assets/Scripts/PIDController.cs
@@ -17,6 +17,17 @@ public class PIDController : MonoBehaviour
     public float distance_wanted;
     bool arrived;
 
+    double Kp_r = 0.005f;
+    double Kd_r = 0.03f;
+    double u_r = 0;
+    int e_r = 0;
+    int e_prev_r = 0;
+    int distance_r = 0;
+    int distance_wanted_r;
+    bool arrived_r = false;
+
+    float timer = 0;
+
     enum Side
     {
         right,
@@ -30,11 +41,18 @@ public class PIDController : MonoBehaviour
         e = 0;
         e_prev = 0;
         arrived = false;
+        //rotate();
+
+        distance_wanted_r = (int)(90f * (0.0525f/motorLeft.WC.radius));
+        Debug.Log("wanted: " + distance_wanted_r);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        timer += Time.deltaTime;
+        /*
         if (sensorFront.getSensorReady()) {
             //Debug.Log("Arrived: " + arrived + " | u = " + u + " | e = " + e);
 
@@ -61,13 +79,74 @@ public class PIDController : MonoBehaviour
         
             e_prev = e;
         }
+        */
+
+        if (timer > 1.0f) {
+            if (!arrived_r) {
+            Debug.Log(distance_r);
+            distance_r = motorLeft.getRotation();
+
+            e_r = distance_wanted_r - distance_r;
+            Debug.Log("e: " + e_r);
+            u_r = Kp_r*e_r + Kd_r*(e_r - e_prev_r);
+            Debug.Log("u: " + u_r);
+
+            } else {
+                u_r = 0;
+            }
+
+            if (u_r <= 0) {
+                Debug.Log("how");
+                motorLeft.setBrake(true);
+                motorRight.setBrake(true);
+                arrived_r = true;
+            }
+
+            motorLeft.setSpeedPercent((float)u_r);
+            motorRight.setSpeedPercent(-(float)u_r);
+        
+            e_prev = e;
+        }
         
     }
 
     // TODO 
-    void rotate(Side side) {
-        if (side == Side.left) {
-            
+    /*
+    void rotate() {
+        double Kp_r = 0.05f;
+        double Kd_r = 0.3f;
+        double u_r = 0;
+        double e_r = 0;
+        double e_prev_r = 0;
+        float distance_r = 0;
+        float distance_wanted_r = 90 * (0.1f/motorLeft.WC.radius);
+        Debug.Log(distance_wanted_r);
+        bool arrived_r = false;
+
+        while(!arrived_r) {
+            Debug.Log(distance_r);
+
+            if (!arrived_r) {
+                distance_r = motorLeft.getRotation();
+
+                e_r = distance_wanted_r - distance_r;
+                u_r = Kp_r*e_r + Kd_r*(e_r - e_prev_r);
+            } else {
+                u_r = 0;
+            }
+
+            if (u <= 0) {
+                motorLeft.setBrake(true);
+                motorRight.setBrake(true);
+                arrived_r = true;
+            }
+
+            motorLeft.setSpeedPercent((float)u_r);
+            motorRight.setSpeedPercent(-(float)u_r);
+        
+            e_prev = e;
         }
+ 
     }
+    */
 }
